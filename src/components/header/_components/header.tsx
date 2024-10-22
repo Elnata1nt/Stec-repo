1ce38ac1cce1e2fccabb688cc7360582d4c83c02
elnatacorrea <elnatacorrea@hotmail.com>
2024-10-22 12:22:17 -0400

    alteraçao da arquitetura do site de seus components

--- src/components/header/_components/header.tsx
+++ src/components/header/_components/header.tsx
@@ -91,7 +91,7 @@ export default function Header() {
                 </NavigationMenuLink>
                 <NavigationMenuLink asChild>
                   <Link
-                    href="/painelchat"
+                    href="/assistance"
                     className="group grid h-auto w-full items-center justify-start gap-1 rounded-md bg-background p-4 text-sm font-black transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
                     prefetch={false}
                   >
